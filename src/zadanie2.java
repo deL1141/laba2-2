2ed1ddb66a4792632a260f16525622fb738fd0eb
Дмитрий <vancor86>
2016-03-21 20:39:37 +0500

    Второе задание

---
+++ src/zadanie2.java
@@ -0,0 +1,17 @@
+/**
+ * Created by deL on 07.03.2016.
+ */
+import java.util.Scanner;
+public class zadanie2 {
+        public static void main(String[] args){
+            int a = 5;
+            int b = 10;
+            int k,m;
+            for(k=1;b>=k;k++){
+                for(m=1;a>m;m++){
+                    System.out.print("8");
+                }
+                System.out.println("8");
+            }
+        }
+    }
